976369ecf11c7e7ca406add12c19aabaf3d98d42
Carlos Castillo <ccastillo.consultant@dot.nyc.gov>
2017-09-01 17:27:42 -0400

    - Total and clear

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -17,6 +17,10 @@ class ListContacts extends Component {
 		this.setState({ query: query.trim() })
 	}
 	
+	clearQuery = () => {
+		this.setState({ query: '' })
+	}
+	
 	render() {
 		
 		const { contacts, onDeleteContacts } = this.props
@@ -46,6 +50,12 @@ class ListContacts extends Component {
 						onChange={(event) => this.updateQuery(event.target.value)}
 					/>
 				</div>
+				{ showingContacts.length !== contacts.length && (
+					<div className="showing-contacts">
+						<span>Now showing {showingContacts.length} of {contacts.length}</span>
+						<button onClick={this.clearQuery}>Show all</button>
+					</div>
+				)}
 				<ol className="contact-list">
 					{showingContacts.map((contact) => (
 						<li key={contact.id} className="contact-list-item">
