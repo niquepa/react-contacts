5331b53966be3f5c55181b6a23ec814fffae9e61
Carlos Castillo <ccastillo.consultant@dot.nyc.gov>
2017-09-03 16:07:22 -0400

    - connect to the API

--- src/App.js
+++ src/App.js
@@ -1,34 +1,23 @@
 import React, { Component } from 'react'
 import ListContacts from './ListContacts'
+import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
 	state = {
-		contacts : [
-			{
-				"id": "ryan",
-				"name": "Ryan Florence",
-				"email": "ryan@reacttraining.com",
-				"avatarURL": "http://localhost:5005/ryan.jpg"
-			},
-			{
-				"id": "michael",
-				"name": "Michael Jackson",
-				"email": "michael@reacttraining.com",
-				"avatarURL": "http://localhost:5005/michael.jpg"
-			},
-			{
-				"id": "tyler",
-				"name": "Tyler McGinnis",
-				"email": "tyler@reacttraining.com",
-				"avatarURL": "http://localhost:5005/tyler.jpg"
-			}
-		]
+		contacts : []
+	}
+	
+	componentDidMount(){
+		ContactsAPI.getAll().then((contacts) => {
+			this.setState({ contacts: contacts})
+		})
 	}
 	
 	removeContact = (contact) =>  {
 		this.setState((prevState) => ({
 			contacts: prevState.contacts.filter((c) => c.id !== contact.id)
 		}))
+		ContactsAPI.remove(contact)
 	}
 	
   render() {
--- src/utils/ContactsAPI.js
+++ src/utils/ContactsAPI.js
@@ -1,4 +1,4 @@
-const api = process.env.REACT_APP_CONTACTS_API_URL || 'http://localhost:5001'
+const api = process.env.REACT_APP_CONTACTS_API_URL || 'http://localhost:5005'
 
 let token = localStorage.token
 
